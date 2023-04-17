f9384705fa766583ec2e98b43c5573e8c1eaadcb
Eunbeann <ds5bean23@gmail.com>
2023-04-17 16:09:30 +0900

    fix: delete trash img

--- breadStore/main.js
+++ breadStore/main.js
@@ -1,42 +1,19 @@
-function breadlist() {
-  fetch("products.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const articleList = document.getElementById("cards");
-      const breadsList = data.breads;
-      for (let i = 0; i < breadsList.length; i++) {
-        const bread = breadsList[i];
+import { breads } from "./products.js";
 
-        const article = document.createElement("article");
-        article.classList.add("section__card");
+const articleList = document.querySelector("#cards");
 
-        const name = document.createElement("h3");
-        name.textContent = bread.name;
-        article.appendChild(name);
+const breadsList = breads.map((bread) => {
+  const hashtags = bread.hashtags
+    .map((tag) => `<li class="section__tag-list">${tag}</li>`)
+    .join(" ");
+  return `
+    <article class="section__card">
+      <h3>${bread.name}</h3>
+      <ul class="section__tag">${hashtags} </ul>
+      <img class="section__img" src="./imgs/${bread.src}"/>
+      <button class="section__like" id="btn${bread.id}" type="button">♥</button>
+    </article>
+  `;
+});
 
-        const ul = document.createElement("ul");
-        ul.classList.add("section__tag");
-        article.appendChild(ul);
-
-        for (let k = 0; k < bread.hashtags.length; k++) {
-          const li = document.createElement("li");
-          li.classList.add("section__tag-list");
-          li.textContent = bread.hashtags[k];
-          ul.appendChild(li);
-        }
-
-        const img = document.createElement("img");
-        img.classList.add("section__img");
-        img.src = "./imgs/" + bread.src;
-        article.appendChild(img);
-
-        const likebtn = document.createElement("button");
-        likebtn.classList.add("section__like");
-        likebtn.type = "button";
-        likebtn.textContent = "♥";
-        article.appendChild(likebtn);
-
-        articleList.appendChild(article);
-      }
-    });
-}
+articleList.innerHTML = breadsList;
