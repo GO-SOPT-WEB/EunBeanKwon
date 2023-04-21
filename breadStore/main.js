9ef290558354854422f5032253a47ddf1d3dcf70
Eunbeann <ds5bean23@gmail.com>
2023-04-21 16:25:42 +0900

    feat: 배열 필터링, 필터링한 배열 HTML

--- breadStore/main.js
+++ breadStore/main.js
@@ -1,26 +1,41 @@
 import { breads } from "./products.js";
 
-const articleList = document.querySelector("#cards");
+// 배열 필터링
+function filterBreadsByType(type) {
+  const filteredBreads = breads.filter((bread) => bread.type === type);
+  return filteredBreads.length > 0 ? filteredBreads : breads;
+}
 
-const breadsList = breads.map((bread) => {
-  const hashtags = bread.hashtags
-    .map((tag) => `<li class="section__tag-list">${tag}</li>`)
-    .join(" ");
-  return `
-    <article class="section__card">
-      <div class="card-modal-container" > 
-        <div> ${hashtags} </div>
-        <button type="button" class="card-modal-close" onclick="closeModal(this)" >X</button>
-      </div>
-      <h3>${bread.name}</h3>
-      <div>
-        <ul class="section__tag">${hashtags} </ul>
-        <button type="button" onclick="openModal(this)" class="section__tag-btn">+</button>
-      </div>
-      <img class="section__img" src="./imgs/${bread.src}" alt="${bread.name}/>
-      <button type="button class="section__like" id="btn${bread.id}" ">♥</button>
-    </article>
-  `;
-});
+// 카드 뿌리기
+function showCards(filterType) {
+const showingList = filterBreadsByType(filterType);
+const articleList = document.querySelector("#cards");
+const breadsList = showingList
+  .map((bread) => {
+    const hashtags = bread.hashtags
+      .map((tag) => `<li class="section__tag-list">${tag}</li>`)
+      .join(" ");
+    return `
+    <article class="${bread.type}">
+        <h3>${bread.name}</h3>
+        <div>
+        <div class="card-modal-container"> 
+        <button type="button" class="card-modal-close">X</button>   
+          <div> ${hashtags} </div>
+        </div>
+          <ul class="section__tag"> ${hashtags} </ul>
+           <button type="button" class="section__tag-btn"> + </button>
+        </div>
+        <img class="section__img" src="./imgs/${bread.src}" alt="${bread.name}"/>
+        <button type="button" class="section__like" id="btn">♥</button>
+    </article>`;
+  })
+  .join(" ");
 
 articleList.insertAdjacentHTML("beforeend", breadsList);
+}
+
+
+
+
+
