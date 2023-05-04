72dc91faed91b1c9d6b5d7a4829f80a18d581d4e
Eunbeann <ds5bean23@gmail.com>
2023-05-04 13:32:12 +0900

    feat: 카드 상수 데이터 추가

---
+++ cardGame/cardGame/src/data/mario.js
@@ -0,0 +1,37 @@
+export const marioData = [{
+    id: 1,
+    alt: "굼바",
+    src : "https://upload.wikimedia.org/wikipedia/en/c/ce/Goomba.PNG",
+},{
+    id: 2,
+    alt: "키노피오",
+    src : "https://upload.wikimedia.org/wikipedia/en/d/d1/Toad_3D_Land.png",
+},{
+    id: 3,
+    alt: "피치공주",
+    src : "https://upload.wikimedia.org/wikipedia/en/1/16/Princess_Peach_Stock_Art.png",
+},{
+    id: 4,
+    alt: "쿠파",
+    src : "https://www.pngplay.com/wp-content/uploads/7/Bowser-Super-Mario-Free-PNG.png",
+},{
+    id: 5,
+    alt: "데이지공주",
+    src : "https://static.wikia.nocookie.net/mariokart/images/9/90/MKT_Artwork_Daisy.png/revision/latest?cb=20211211153952",
+},{
+    id: 6,
+    alt: "버섯",
+    src : "https://www.pngplay.com/wp-content/uploads/12/Mario-Mushroom-Transparent-PNG.png",
+},{
+    id: 7,
+    alt: "플라워",
+    src : "https://static.wikia.nocookie.net/mario/images/2/24/NSMBUD_Fire_Flower.png/revision/latest?cb=20190730173640",
+},{
+    id: 8,
+    alt: "마리오",
+    src : "https://static.wikia.nocookie.net/mario/images/e/e3/MPS_Mario.png/revision/latest/thumbnail/width/360/height/360?cb=20220814154953",
+},{
+    id: 9,
+    alt: "루이지",
+    src : "https://www.pngmart.com/files/2/Luigi-PNG-Photos.png",
+}]
