0555b53b05d9f3b341477b7b7926cc06f61332b2
Eunbeann <ds5bean23@gmail.com>
2023-05-05 23:30:01 +0900

    feat: 상수 데이터 수정, 레벨 상수 추가

--- cardGame/cardGame/src/data/mario.js
+++ cardGame/cardGame/src/data/mario.js
@@ -2,36 +2,45 @@ export const marioData = [{
     id: 1,
     alt: "굼바",
     src : "https://upload.wikimedia.org/wikipedia/en/c/ce/Goomba.PNG",
+    matched: false
 },{
     id: 2,
     alt: "키노피오",
     src : "https://upload.wikimedia.org/wikipedia/en/d/d1/Toad_3D_Land.png",
+    matched: false
 },{
     id: 3,
     alt: "피치공주",
     src : "https://upload.wikimedia.org/wikipedia/en/1/16/Princess_Peach_Stock_Art.png",
+    matched: false
 },{
     id: 4,
     alt: "쿠파",
     src : "https://www.pngplay.com/wp-content/uploads/7/Bowser-Super-Mario-Free-PNG.png",
+    matched: false
 },{
     id: 5,
     alt: "데이지공주",
-    src : "https://static.wikia.nocookie.net/mariokart/images/9/90/MKT_Artwork_Daisy.png/revision/latest?cb=20211211153952",
+    src : "https://upload.wikimedia.org/wikipedia/en/b/bd/Daisy_%28Super_Mario_Party%29.png",
+    matched: false
 },{
     id: 6,
     alt: "버섯",
     src : "https://www.pngplay.com/wp-content/uploads/12/Mario-Mushroom-Transparent-PNG.png",
+    matched: false
 },{
     id: 7,
-    alt: "플라워",
-    src : "https://static.wikia.nocookie.net/mario/images/2/24/NSMBUD_Fire_Flower.png/revision/latest?cb=20190730173640",
+    alt: "요시",
+    src : "https://ssb.wiki.gallery/images/thumb/2/2a/Yoshi_MP10.png/1200px-Yoshi_MP10.png",
+    matched: false
 },{
     id: 8,
     alt: "마리오",
-    src : "https://static.wikia.nocookie.net/mario/images/e/e3/MPS_Mario.png/revision/latest/thumbnail/width/360/height/360?cb=20220814154953",
+    src : "https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png",
+    matched: false
 },{
     id: 9,
     alt: "루이지",
     src : "https://www.pngmart.com/files/2/Luigi-PNG-Photos.png",
+    matched: false
 }]
