238f50507309fa9132f7af08553c4fd5dbe01fb2
Eunbeann <ds5bean23@gmail.com>
2023-05-05 23:31:28 +0900

    styles: 전역 스타일 설정

--- cardGame/cardGame/src/App.jsx
+++ cardGame/cardGame/src/App.jsx
@@ -1,20 +1,58 @@
-import Header from './components/header';
-import { createGlobalStyle } from 'styled-components'
+
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import theme from './styles/theme'
 import reset from 'styled-reset'
-
-
-const GlobalStyle = createGlobalStyle`
-    ${reset}
-`;
+import GmarketSansBold from './assets/fonts/GmarketSansBold.ttf'
+import GmarketSansMedium from './assets/fonts/GmarketSansMedium.ttf'
+import GmarketSansLight from './assets/fonts/GmarketSansLight.ttf'
+import CardGame from './pages/CardGame'
 
 const App = () => {
 
   return (
     <>
+    <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <Header />
+    <CardGame />
+    </ThemeProvider>
     </>
   )
 }
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  @font-face {
+      font-family: 'GmarketSansMedium';
+      src: url(${GmarketSansMedium}) format('ttf');
+      font-weight: normal;
+      font-style: normal; 
+  }
+
+  @font-face {
+      font-family: 'GmarketSansBold';
+      src: url(${GmarketSansBold}) format('ttf');
+      font-weight: normal;
+      font-style: normal; 
+  }
+
+  @font-face {
+      font-family: 'GmarketSansLight';
+      src: url(${GmarketSansLight}) format('ttf');
+      font-weight: normal;
+      font-style: normal; 
+  }
+
+  body, html {
+    font-family: 'GmarketSansMedium';
+    font-size: 16px;
+  }
+
+  button {
+    all:unset;
+    :hover {
+        cursor: pointer;
+  }
+}
+`;
 export default App
