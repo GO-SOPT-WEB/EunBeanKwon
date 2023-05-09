4459516d0011d1c0beaacf96c1decaf7383bb759
Eunbeann <ds5bean23@gmail.com>
2023-05-10 01:42:40 +0900

    feat: 카드 한장만 뒤집히게 + 매치 될 때 matched 값 변경

--- cardGame/cardGame/src/components/CardContainer.jsx
+++ cardGame/cardGame/src/components/CardContainer.jsx
@@ -2,36 +2,31 @@ import styled from "styled-components";
 import SingleCard from "./SingleCard";
 import { useEffect, useState } from "react";
 
-const CardContainer = ({cardsNum, }) => {
+const CardContainer = ({cardsNum}) => {
 
-  const [matched, setMatched] = useState(0);
+  const [score, setScore] = useState(0);
   const [cards, setCards] = useState([]);
 
   const [firstCard, setFirstCard] = useState(null);
   const [secondCard, setSecondCard] = useState(null);
 
   const [disabled, setDisabled] = useState(false);
-  const [isOpen, setIsOpen] = useState(Array(cardsNum.length).fill(false));
-
-  
 
   const cardClicked = (card) => {
     firstCard? setSecondCard(card): setFirstCard(card);
-    const updatedIsOpen = [...isOpen]; 
-    updatedIsOpen[card.id] = true; 
-    setIsOpen(updatedIsOpen); 
 }
 
+// loadCards
 
   //선택된 카드들 비교
   useEffect(() => {
-    console.log(isOpen)
     if (firstCard && secondCard) {
       setDisabled(true);
       if (firstCard.src === secondCard.src) {
         setCards((prevCards) => {
           return prevCards.map((card) => {
             if (card.src === firstCard.src) {
+              setScore(score + 1);
               return { ...card, matched: true };
             } else {
               return card;
@@ -47,7 +42,7 @@ const CardContainer = ({cardsNum, }) => {
   }, [firstCard, secondCard]);
 
 
-  // reset Choice
+  // 클릭된 카드들 점부 초기화
   const resetTurn = () => {
     setFirstCard(null);
     setSecondCard(null);
@@ -55,18 +50,17 @@ const CardContainer = ({cardsNum, }) => {
   }
 
 
+
   return (
     <StCardContainer>
-      {cardsNum.map((card,index) => {
-
+      {cards.map((card,index) => {
         return ( 
           <SingleCard 
             key={`${card.id}${index}`}
             card={card}
             cardClicked={cardClicked}
-            flipped={card === firstCard || card === secondCard }
+            flipped={card === firstCard || card === secondCard || card.matched }
             disabled={disabled}
-            idOpen={isOpen}
           />
         )
       })}
@@ -81,7 +75,6 @@ const StCardContainer = styled.div `
 display: flex;
 flex-wrap: wrap;
 justify-content: center;
-width: 60vw;
 
 flex-wrap: wrap;
 justify-content: center;
