9a279a9d36efa4f49bdc5662b5901e05c6065fd7
Eunbeann <ds5bean23@gmail.com>
2023-05-10 22:04:15 +0900

    desgin: 전역 테마 색 수정, 폰트 수정

--- weather-forecast/src/styles/GlobalStyles.jsx
+++ weather-forecast/src/styles/GlobalStyles.jsx
@@ -9,6 +9,7 @@ const GlobalStyles = createGlobalStyle`
     }
     *{
         box-sizing: border-box;
+        font-family: 'NanumSquareRoundOTF', sans-serif;
     }
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
     a, dl, dt, dd, ol, ul, li, form, label, table{
@@ -35,7 +36,6 @@ const GlobalStyles = createGlobalStyle`
     }
     body{
         line-height: 1;
-        font-family: 'NanumSquareRoundOTF', sans-serif;
         margin-bottom: 100px;
     }
     ol, ul{
--- weather-forecast/src/styles/theme.jsx
+++ weather-forecast/src/styles/theme.jsx
@@ -1,8 +1,9 @@
 const theme = {
-    backgroundColor: "#FBF3F2",
-    pointColor:"#FB991C",
-    blue:"#197799",
-    darkBlue:"#032539"
+    backgroundColor: "#FEFAE0", //whitE 처럼 쓰기
+    beige:"#FAEDCD",
+    darkBeige:"#D4A373",
+    muteGreen:"#CCD5AE",
+    lightGreen:"#E9EDC9"
   };
    
   export default theme;
