735543429b9d9a611a8083dc877ba312cee61af6
Eunbeann <ds5bean23@gmail.com>
2023-06-02 13:34:01 +0900

    [ chore ] styled-components 설치

--- TS-cardGame/ts-card/src/App.tsx
+++ TS-cardGame/ts-card/src/App.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import reset from "styled-reset";
 
 const App = () => {
   return (
@@ -7,4 +9,43 @@ const App = () => {
   )
 }
 
+const GlobalStyle = cr`
+  ${reset}
+
+  @font-face {
+      font-family: 'GmarketSansMedium';
+      src: url(${GmarketSansMedium}) format('ttf');
+      font-weight: normal;
+      font-style: normal; 
+  }
+
+  @font-face {
+      font-family: 'GmarketSansBold';
+      src: url(${GmarketSansBold}) format('ttf');
+      font-weight: normal;
+      font-style: normal; 
+  }
+
+  @font-face {
+      font-family: 'GmarketSansLight';
+      src: url(${GmarketSansLight}) format('ttf');
+      font-weight: normal;
+      font-style: normal; 
+  }
+
+  body, html {
+    font-family: 'GmarketSansMedium';
+    font-size: 16px;
+  }
+
+  button {
+    all:unset;
+    :hover {
+        cursor: pointer;
+  }
+}
+`;
+
 export default App
+
+
