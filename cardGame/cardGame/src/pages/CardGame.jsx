8dad8f43e4e1f7168798119e04bf3b98f7b6ed09
Eunbeann <ds5bean23@gmail.com>
2023-06-05 20:58:49 +0900

    [ style ] 코드 정리

--- cardGame/cardGame/src/pages/CardGame.jsx
+++ cardGame/cardGame/src/pages/CardGame.jsx
@@ -1,69 +1,58 @@
-import styled, {keyframes , css} from 'styled-components';
+import styled, { keyframes, css } from "styled-components";
 import { useState, useEffect } from "react";
 import CardContainer from "../components/CardContainer";
 import ResetBtn from "../components/ResetBtn";
 import { Level } from "../data/level";
 import Modal from "../components/Modal";
 
-
-// 카드 데이터 가지고 오기 
-import {
-  EasyVersion,
-  NormalVersion,
-  HardVersion,
-} from "../utils/ShuffledCard";
-
+// 카드 데이터 가지고 오기
+import { EasyVersion, NormalVersion, HardVersion } from "../utils/ShuffledCard";
 
 const CardGame = () => {
-
-
-  const [cardsNum, setCardsNum] = useState(EasyVersion)
-  const [nowLevel, setNowLevel] = useState("EASY")
+  const [cardsNum, setCardsNum] = useState("EasyVersion");
+  const [nowLevel, setNowLevel] = useState("EASY");
 
   const [score, setScore] = useState(0);
   const [animateScore, setAnimateScore] = useState(false);
 
   const [modalOn, setModalOn] = useState(false);
 
-  const [firstCard, setFirstCard] = useState();
+  const [firstCard, setFirstCard] = useState(null);
   const [secondCard, setSecondCard] = useState(null);
 
   const [disabled, setDisabled] = useState(false);
 
-
   useEffect(() => {
-    if (score === cardsNum.length/2) {
+    if (score === cardsNum.length / 2) {
       setModalOn(true);
     }
-  }, [score,setModalOn,modalOn]);
+  }, [score, setModalOn, modalOn]);
 
-
-// 레벨 버튼 클릭 시 카드 수 변경
+  // 레벨 버튼 클릭 시 카드 수 변경
   const ClickedLv = (e) => {
-    setScore(0)
-    setNowLevel(e.target.value)
+    setScore(0);
+    setNowLevel(e.target.value);
     switch (e.target.value) {
-      case 'EASY' :
-        return setCardsNum(EasyVersion)
-      case 'NORMAL' :
-        return setCardsNum(NormalVersion)
-      case 'HARD' :
-        return setCardsNum(HardVersion)
-      }
+      case "EASY":
+        return setCardsNum(EasyVersion);
+      case "NORMAL":
+        return setCardsNum(NormalVersion);
+      case "HARD":
+        return setCardsNum(HardVersion);
     }
+  };
 
-    // 카드 클릭시 첫번째로 클릭한 카드, 두번째로 클릭한 카드 넣어주기
-    const cardClicked = (card) => {
-      firstCard? setSecondCard(card): setFirstCard(card);
-  }
+  // 카드 클릭시 첫번째로 클릭한 카드, 두번째로 클릭한 카드 넣어주기
+  const cardClicked = (card) => {
+    firstCard ? setSecondCard(card) : setFirstCard(card);
+  };
 
   // 클릭된 카드들 점부 초기화
   const resetTurn = () => {
     setFirstCard(null);
     setSecondCard(null);
     setDisabled(false);
-  }
-
+  };
 
   //선택된 카드들 비교해서 같으면 matched 값 부여
   useEffect(() => {
@@ -88,8 +77,7 @@ const CardGame = () => {
     }
   }, [firstCard, secondCard]);
 
-
-// 점수 애니메이션
+  // 점수 애니메이션
   useEffect(() => {
     setAnimateScore(true);
     const timeoutId = setTimeout(() => setAnimateScore(false), 500);
@@ -98,88 +86,90 @@ const CardGame = () => {
 
   return (
     <>
-    <Modal modalOn={modalOn} setModalOn={setModalOn} />
-    <ResetBtn />
-    <StHeader>
+      <Modal modalOn={modalOn} setModalOn={setModalOn} />
+      <ResetBtn />
+      <StHeader>
         <p> Match the MARIO! 🍄 </p>
-        <StScore animate={animateScore} > {score}/{cardsNum.length/2} </StScore>
-    </StHeader>
-    <StGameContainer>
-      <StLevenContainer>
-      {Level.map(level => {
-          return (
-              <StLevelBtn 
-                  key={level.id} 
-                  value={level.lv} 
-                  type="button" 
-                  className={"btn"+(level.lv == nowLevel ? " active" : "")} 
-                  onClick={ClickedLv}>
-                  {level.lv}
+        <StScore animate={animateScore}>
+          {" "}
+          {score}/{cardsNum.length / 2}{" "}
+        </StScore>
+      </StHeader>
+      <StGameContainer>
+        <StLevenContainer>
+          {Level.map((level) => {
+            return (
+              <StLevelBtn
+                key={level.id}
+                value={level.lv}
+                type="button"
+                className={"btn" + (level.lv == nowLevel ? " active" : "")}
+                onClick={ClickedLv}
+              >
+                {level.lv}
               </StLevelBtn>
-          )
-      })}
-      </StLevenContainer>
-        <CardContainer 
+            );
+          })}
+        </StLevenContainer>
+        <CardContainer
           cardsNum={cardsNum}
           cardClicked={cardClicked}
           firstCard={firstCard}
           secondCard={secondCard}
-          disabled={disabled} />
-    </StGameContainer>
+          disabled={disabled}
+        />
+      </StGameContainer>
     </>
-  )
-}
+  );
+};
 
 export default CardGame;
 
 const StHeader = styled.header`
-    display: flex;
-    height: 20vh;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  height: 20vh;
+  flex-direction: column;
+  justify-content: center;
 
-    > p {
-    color:${({theme}) => theme.color.red};
-    font-size: 3rem; 
+  > p {
+    color: ${({ theme }) => theme.color.red};
+    font-size: 3rem;
     text-align: center;
     font-weight: 600;
-    }
-`
+  }
+`;
 
 const StGameContainer = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    height: 100%;
-    padding-top: 2.5rem;  
+  height: 100%;
+  padding-top: 2.5rem;
 
-    background-color: ${({theme}) => theme.color.deepBlue};
-`
+  background-color: ${({ theme }) => theme.color.deepBlue};
+`;
 
 const StLevenContainer = styled.nav`
-    display: flex;
-    justify-content: center;
-`
-
-
+  display: flex;
+  justify-content: center;
+`;
 
 const StLevelBtn = styled.button`
-    width: 5rem;
-    margin: 1rem 1rem;
-    background-color:${({theme}) => theme.color.yellow};
-    padding: 1rem 1rem;
-    
-    text-align: center;
-    border-radius: 1rem;
+  width: 5rem;
+  margin: 1rem 1rem;
+  background-color: ${({ theme }) => theme.color.yellow};
+  padding: 1rem 1rem;
 
-    box-shadow: 0.2rem 0.2rem ${({theme}) => theme.color.blue};
-    &.active {
-        background-color: ${({theme}) => theme.color.blue};
-        box-shadow: 0.2rem 0.2rem ${({theme}) => theme.color.yellow};
-    }
-`
+  text-align: center;
+  border-radius: 1rem;
 
+  box-shadow: 0.2rem 0.2rem ${({ theme }) => theme.color.blue};
+  &.active {
+    background-color: ${({ theme }) => theme.color.blue};
+    box-shadow: 0.2rem 0.2rem ${({ theme }) => theme.color.yellow};
+  }
+`;
 
 const blinkScore = keyframes`
 0% {
@@ -194,11 +184,15 @@ const blinkScore = keyframes`
 `;
 
 const StScore = styled.div`
-  color:${({theme}) => theme.color.green};
-  font-size: 2.6rem; 
+  color: ${({ theme }) => theme.color.green};
+  font-size: 2.6rem;
   text-align: center;
   font-weight: 600;
   margin-top: 1.5rem;
-  animation: ${({ animate }) => animate ? css`${blinkScore} 1s ` : 'none'};
-
-`
+  animation: ${({ animate }) =>
+    animate
+      ? css`
+          ${blinkScore} 1s
+        `
+      : "none"};
+`;
