996d8d37a8455f2d17b4f1d88bbf467f3c56ff39
Eunbeann <ds5bean23@gmail.com>
2023-06-06 17:41:23 +0900

    [ init ] TS 전환용 파일 끌어오기

--- TS-cardGame/ts-card/src/utils/ShuffledCard.tsx
+++ TS-cardGame/ts-card/src/utils/ShuffledCard.tsx
@@ -1,13 +1,20 @@
-import { marioData } from "../data/mario";
+import { marioData } from '../data/mario';
 
-export interface ShuffledCardInfo {
-    arr: string[];
-}
-
-export const Shuffled = (arr) => arr.sort(() => Math.random() - 0.5)
-
-console.log(typeof(marioData))
+export const Shuffled = (arr) => arr.sort(() => Math.random() - 0.5);
 
 export let ShuffledMario = Shuffled(marioData);
 
-// export const
+export const EasyVersion = Shuffled([...ShuffledMario.slice(0, 5), ...ShuffledMario.slice(0, 5)]).map((randomId) => ({
+    ...randomId,
+    id: Math.random() * 100,
+}));
+
+export const NormalVersion = Shuffled([...ShuffledMario.slice(0, 7), ...ShuffledMario.slice(0, 7)]).map((randomId) => ({
+    ...randomId,
+    id: Math.random(),
+}));
+
+export const HardVersion = Shuffled([...ShuffledMario.slice(0, 9), ...ShuffledMario.slice(0, 9)]).map((randomId) => ({
+    ...randomId,
+    id: Math.random(),
+}));
