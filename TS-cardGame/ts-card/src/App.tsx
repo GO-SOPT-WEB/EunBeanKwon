0b3320ba0c986f42c1c6d6f76ace744a63d13482
Eunbeann <ds5bean23@gmail.com>
2023-06-06 17:51:01 +0900

    [ chore ] @types/recoil

--- TS-cardGame/ts-card/src/App.tsx
+++ TS-cardGame/ts-card/src/App.tsx
@@ -1,21 +1,23 @@
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
-import reset from "styled-reset";
-import GmarketSansBold from './assets/fonts/GmarketSansBold.ttf'
-import GmarketSansMedium from './assets/fonts/GmarketSansMedium.ttf'
-import GmarketSansLight from './assets/fonts/GmarketSansLight.ttf'
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import GmarketSansBold from './assets/fonts/GmarketSansBold.ttf';
+import GmarketSansMedium from './assets/fonts/GmarketSansMedium.ttf';
+import GmarketSansLight from './assets/fonts/GmarketSansLight.ttf';
 import CardGame from './pages/CardGame';
 import theme from './styles/theme';
 
 const App = () => {
-  return (
-    <>
-    <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <CardGame />
-    </ThemeProvider>
-    </>
-  )
-}
+    return (
+        <>
+            <RecoilRoot>
+                <ThemeProvider theme={theme}>
+                    <GlobalStyle />
+                    <CardGame />
+                </ThemeProvider>
+            </RecoilRoot>
+        </>
+    );
+};
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -54,6 +56,4 @@ const GlobalStyle = createGlobalStyle`
 }
 `;
 
-export default App
-
-
+export default App;
